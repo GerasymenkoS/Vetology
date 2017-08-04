3a94e32ab2225779c81e52d1b3ae778acf68adc3
Stanislav Gerasymenko <gstanislav3@gmail.com>
2017-08-04 13:47:41 +0300

    Build menu and fix bugs

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import os
 import random
 import string
 
+import elasticsearch
 from flask import Flask
-from flask import flash
-from flask import redirect
 from flask import render_template
 from flask import send_from_directory
 from werkzeug.utils import secure_filename
 
-from search import search_file, load_file
+from search import WorkWithSignatures
 from flask import request
 import csv, io
+from ast import literal_eval
 
 UPLOAD_FOLDER = '/home/incode51/PycharmProjects/ImageSearch/images/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
@@ -19,6 +19,7 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 app = Flask(__name__, static_url_path='/images/')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+sign = WorkWithSignatures()
 
 @app.route('/images/<path:path>')
 def send_iamges(path):
@@ -49,7 +50,7 @@ def load_csv_file():
             path_reader = csv.reader(csv_file, delimiter=' ', quotechar='|')
             for path in path_reader:
                 try:
-                    load_file(path[0])
+                    sign.load_file(path[0])
                 except Exception as e:
                     print("Wrond " + str(e))
             return "OK"
@@ -58,8 +59,40 @@ def load_csv_file():
     else:
         return render_template('load_csv.html')
 
+
+@app.route("/params/", methods=['GET', 'POST'])
+def params():
+    if request.method == 'POST':
+        params_dict = {'n_grid': 9,
+                       'crop_percentile': (5, 95),
+                       'P': None,
+                       'diagonal_neighbors': True,
+                       'identical_tolerance': 2/255,
+                       'n_levels': 2}
+        f = request.form
+        try:
+            params_dict['n_grid'] = int(f['n_grid'])
+            params_dict['crop_percentile'] = literal_eval(f['crop_percentile'])
+            params_dict['P'] = int(f['P']) if f['P'] else None
+            params_dict['diagonal_neighbors'] = bool(f.get('diagonal_neighbors'))
+            params_dict['identical_tolerance'] = float(f['identical_tolerance'])
+            params_dict['n_levels'] = int(f['n_levels'])
+        except ValueError as e:
+            return "Incorrect params"
+        except Exception as e:
+            return "Wrong"
+        else:
+            sign.reload_params(params_dict)
+            return "OK"
+    else:
+        p = sign.get_all_params()
+        print(p)
+        return render_template('params.html', **sign.get_all_params())
+
+
 @app.route("/load_images/", methods=['GET', 'POST'])
 def load_new_file():
+    print(request.method)
     if request.method == 'POST':
         files = request.files.getlist('file')
         if files:
@@ -71,20 +104,31 @@ def load_new_file():
                     filename = str(salt) + secure_filename(file.filename)
                     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
                     file.save(file_path)
-                    load_file(file_path)
+                    sign.load_file(file_path)
             return "OK"
         else:
             return render_template('load.html')
     else:
         return render_template('load.html')
 
+@app.route("/clear/", methods=['GET'])
+def clear():
+    sign.clear_db()
+    return "Deleted"
+
+
+
 @app.route("/search/", methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
         if request.files['file']:
             f = request.files['file'].read()
-            result = search_file(f)
-            return render_template('result.html', result=result)
+            try:
+                result = sign.search_file(f)
+            except elasticsearch.exceptions.NotFoundError as e:
+                return "DB is empty"
+            else:
+                return render_template('result.html', result=result)
         else:
             return render_template('search.html')
     else:
--- search.py
+++ search.py
@@ -1,11 +1,53 @@
 from elasticsearch import Elasticsearch
 from image_match.elasticsearch_driver import SignatureES
 
-es = Elasticsearch()
-ses = SignatureES(es)
 
-def load_file(path):
-    ses.add_image(path)
 
-def search_file(file_bytes):
-    return ses.search_image(file_bytes, bytestream=True)
+class WorkWithSignatures():
+    n_grid = 9
+    crop_percentile = (5, 95)
+    P = None
+    diagonal_neighbors = True
+    identical_tolerance = 2/255
+    n_levels = 2
+
+    es = Elasticsearch(
+        ['2f93f0f9514feb202fd1f7bf0c156606.us-west-2.aws.found.io'],
+        http_auth=('elastic', '5WaeXxhCvdzYvlmj7PvG4l69'),
+        port=9243,
+        use_ssl=True,
+        verify_certs=True
+    )
+
+    ses = SignatureES(es, n_grid=n_grid, crop_percentile=crop_percentile, diagonal_neighbors=diagonal_neighbors,
+                      identical_tolerance=identical_tolerance, n_levels=n_levels)
+
+    def clear_db(self):
+        self.es.indices.delete(index='images', ignore=[400, 404])
+        self.es = Elasticsearch()
+        self.ses = SignatureES(self.es, n_grid=self.n_grid, crop_percentile=self.crop_percentile, diagonal_neighbors=self.diagonal_neighbors,
+                      identical_tolerance=self.identical_tolerance, n_levels=self.n_levels)
+
+    def reload_params(self, params):
+        self.ses = SignatureES(self.es, **params)
+        self.n_grid = params['n_grid']
+        self.crop_percentile = params['crop_percentile']
+        self.P = params['P']
+        self.diagonal_neighbors = params['diagonal_neighbors']
+        self.identical_tolerance = params['identical_tolerance']
+        self.n_levels = params['n_levels']
+
+    def get_all_params(self):
+        return {'n_grid': self.n_grid,
+               'crop_percentile': self.crop_percentile,
+               'P': self.P,
+               'diagonal_neighbors': self.diagonal_neighbors,
+               'identical_tolerance': self.identical_tolerance,
+               'n_levels': self.n_levels}
+
+
+    def load_file(self, path):
+        self.ses.add_image(path)
+
+    def search_file(self, file_bytes):
+        return self.ses.search_image(file_bytes, bytestream=True)
